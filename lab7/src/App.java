00b7eb0c645e8ac33d65628b62861f115b560f95
SillyCatto <raiyanmuhtasim@gmail.com>
2024-11-14 22:50:24 +0600

    added another interface

--- lab7/src/App.java
+++ lab7/src/App.java
@@ -6,17 +6,21 @@ public class App {
     private Scanner scanner;
     private boolean isRunning;
     private IUserReader userReader;
+    private IUserReader adminUserReader;
     private String filePath;
     private ArrayList<User> users;
+    private ArrayList<User> adminUsers;
 
-    public App(String filePath) {
+    public App(String adminFilePath, String userFilePath) {
         this.userService = new UserServices();
         this.scanner = new Scanner(System.in);
         this.isRunning = true;
         this.filePath = filePath;
         this.userReader = ReaderFactory.createReader(filePath);
+        this.adminUserReader = ReaderFactory.createReader(filePath);
     }
     public void start() {
         users = userReader.readUser(filePath);
+        adminUsers = adminUserReader.readUser(filePath);
     }
 }
--- lab7/src/FileUtil.java
+++ lab7/src/FileUtil.java
@@ -1,5 +1,12 @@
 public class FileUtil {
-    public static String getFileNameWithExtension(String fileName) {
-        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    public static String getFileNameWithExtension(String filePath) {
+        // Find the last occurrence of the file separator (either \ or /)
+        int lastSeparatorIndex = Math.max(filePath.lastIndexOf('/'), filePath.lastIndexOf('\\'));
+
+        if (lastSeparatorIndex != -1 && lastSeparatorIndex < filePath.length() - 1) {
+            return filePath.substring(lastSeparatorIndex + 1);
+        }
+
+        return filePath;
     }
 }
